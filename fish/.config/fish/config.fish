39f506885efe253637c030e8d4c3986d96d7b987
Blackbeard <67589385+blackbeard173@users.noreply.github.com>
2023-04-06 14:04:47 +0530

    fish update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,11 @@ set -U fish_greeting
 # aliases
 alias ls "exa -la"
 alias v "nvim"
+alias fishy "nvim $HOME/.config/fish/config.fish" 
+
+fish_add_path $HOME/.local/bin/
+
+if set -q ZELLIJ
+else
+  zellij
+end
