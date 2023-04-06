1113e40f9a138f78966fe90aa867b9bb86e25784
Blackbeard <67589385+blackbeard173@users.noreply.github.com>
2023-04-06 14:03:39 +0530

    change helix config

--- helix/.config/helix/lang-servers.sh
+++ helix/.config/helix/lang-servers.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-npm install -g typescript
+npm install -g typescript typescript-language-server
 
 npm i -g bash-language-server
 
 npm i -g vscode-langservers-extracted # css,html,json
+
+npm i -g @prisma/language-server
 
 # C/C++ -> clangd
 sudo nala install clangd 
 
 # Markdown
 # https://github.com/artempyanykh/marksman
+nix-env -iA nixpkgs.marksman
 
 # Toml
 nix-env -iA nixpkgs.taplo
+
+nix-env -iA nixpkgs.lldb
