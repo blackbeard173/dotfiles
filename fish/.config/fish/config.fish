257d2c85a8648d91f6e46bbbe881c63fe3fec87b
Blackbeard <67589385+blackbeard173@users.noreply.github.com>
2023-06-12 21:49:59 +0530

    refactor: remove redundant nix source

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,4 @@
 set -U fish_greeting
-
-if test -e /home/ubuntu/.nix-profile/etc/profile.d/nix.fish; . /home/ubuntu/.nix-profile/etc/profile.d/nix.fish;
-
 starship init fish | source
 zoxide init fish | source
 fnm env --use-on-cd | source
