ea1edc8f22ce8817723ac059d0863ae6cde032da
Blackbeard <67589385+blackbeard173@users.noreply.github.com>
2024-02-10 12:23:21 +0530

    feat(nvim): jetbrains font, tokyo night theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,10 +2,10 @@ local wezterm = require("wezterm")
 
 local config = {
 	audible_bell = "Disabled",
-	color_scheme = "carbonfox",
+	color_scheme = "Tokyo Night",
 	default_cursor_style = "BlinkingBar",
-	font = wezterm.font("Dank Mono", { weight = "Bold" }),
-	font_size = 14.0,
+	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" }),
+	font_size = 13.0,
 	hide_tab_bar_if_only_one_tab = true,
 	use_fancy_tab_bar = false,
 	window_close_confirmation = "NeverPrompt",
