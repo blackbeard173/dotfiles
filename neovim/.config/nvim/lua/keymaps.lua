58eb056fe787cff3891274586eb82a838b58c1d3
Blackbeard <67589385+blackbeard173@users.noreply.github.com>
2024-02-10 12:22:23 +0530

    feat(nvim): disable arrow keys in normal mode

--- neovim/.config/nvim/lua/keymaps.lua
+++ neovim/.config/nvim/lua/keymaps.lua
@@ -1,2 +1,7 @@
 -- keymaps
+vim.keymap.set("n", "<Up>", "<Nop>")
+vim.keymap.set("n", "<Down>", "<Nop>")
+vim.keymap.set("n", "<Left>", "<Nop>")
+vim.keymap.set("n", "<Right>", "<Nop>")
+
 vim.keymap.set("n", "<Leader>e", "<cmd>Lexplore!<cr>")
