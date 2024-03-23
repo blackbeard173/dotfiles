6caa93299c450b105b91cd6d6ec1488e809df9fa
Blackbeard <67589385+blackbeard173@users.noreply.github.com>
2024-03-23 23:53:03 +0530

    feat(wezterm): font,colors change

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,13 +2,20 @@ local wezterm = require("wezterm")
 
 local config = {
 	audible_bell = "Disabled",
-	color_scheme = "Tokyo Night",
+	color_scheme = "rose-pine",
 	default_cursor_style = "BlinkingBar",
-	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular" }),
-	font_size = 13.0,
+	font = wezterm.font("Berkeley Mono", { weight = "Regular" }),
+	font_size = 14.0,
 	hide_tab_bar_if_only_one_tab = true,
 	use_fancy_tab_bar = false,
 	window_close_confirmation = "NeverPrompt",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	window_decorations = "RESIZE",
 }
 
 return config
