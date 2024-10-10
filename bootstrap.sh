fdf20c6e6b3a089ea0338b89e6245a8e91b32f20
BlackBeard <67589385+blackbeard173@users.noreply.github.com>
2024-10-10 23:47:54 +0530

    feat(bootstrap): install gum

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,11 @@ if [ -d "$HOME/dotfiles" ]; then
 else
 	gh repo clone https://github.com/blackbeard173/dotfiles >/dev/null
 fi
+
+# 2. Install Gum.
+cd /tmp || exit
+GUM_VERSION="0.14.5"
+wget -qO gum.rpm "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum-${GUM_VERSION}-1.x86_64.rpm"
+sudo dnf install -y ./gum.rpm
+rm gum.rpm
+cd - || exit
