7920da6354172a3cd42a759f492a96a9e018935b
BlackBeard <67589385+blackbeard173@users.noreply.github.com>
2024-10-10 21:34:13 +0530

    feat(wezterm): switch to Dank Mono font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 local config = {}
 config.color_scheme = "Aura (Gogh)"
+config.font = wezterm.font("DankMono Nerd Font")
 config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
 return config
