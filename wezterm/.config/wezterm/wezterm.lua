c1e25d480fa67f0e81cc0cf356c06bc820fc189a
BlackBeard <67589385+blackbeard173@users.noreply.github.com>
2024-10-22 19:08:09 +0530

    feat: (wezterm) disable fancy tab bar

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,9 @@
 local wezterm = require("wezterm")
 local config = {}
 config.color_scheme = "Aura (Gogh)"
-config.font = wezterm.font("DankMono Nerd Font")
+config.font = wezterm.font("DankMono Nerd Font" , { weight = 'Bold' } )
 config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = false
 return config
